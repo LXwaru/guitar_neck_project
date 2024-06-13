d0af3d76b4a9bda6bf732fb06ff9435b37d130ca
Alexander Rubin <your@email.here>
2024-06-13 10:26:52 -0500

    experimenting with making the viteConfig base an array of acceptable bases

--- guitar_neck_project/vite.config.js
+++ guitar_neck_project/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/guitar_neck_project/",
+  base: [
+    "/guitar_neck_project/", 
+    "/guitar_neck_project/GuitarNeckNotes/",
+    "/guitar_neck_project/GuitarNeckIntervals/",
+    "/guitar_neck_project/GuitarNeckTriads/"
+  ]
   plugins: [react()],
 })
